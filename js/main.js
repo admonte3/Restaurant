065c5a3c10dea0f7924538de489ba0ca5bbd2291
Adrian Montes <admonte3@gmail.com>
2019-07-22 20:05:28 -0400

    Fixed slides

--- js/main.js
+++ js/main.js
@@ -23,14 +23,3 @@ function plusSlides(n) {
   }
 
 
-
-//Mobile Menu
-var menuBtn = document.getElementsByClassName('menu-btn')
-var mobileMenu = document.getElementsByClassName('mobile-menu')
-
-var clickedBtn = function(){
-    mobileMenu[0].classList.toggle('active')
-}
-menuBtn[0].addEventListener('click', clickedBtn)
-
-console.log(menuBtn[0])
